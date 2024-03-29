ed8741fb995fd496594d8fa3df49726f1118d879
venkatthadi <venkat@mobicip.com>
2024-03-29 13:09:25 +0530

    29.03.24 classes example

---
+++ class_ex.rb
@@ -0,0 +1,71 @@
+class Song
+	# variables
+	@@plays = 0
+
+	#return name of the song
+	def name
+		@name
+	end
+
+	#return artist of the song
+	def artist
+		@artist
+	end
+
+	#return duation of the song
+	def duration
+		@duration
+	end
+
+	def initialize(name, artist, duration)
+		@name = name
+		@artist = artist
+		@duration = duration
+		@plays = 0
+	end
+
+	def play
+		@plays += 1
+		@@plays += 1
+		"This song: #@plays plays. Total #@@plays plays."
+	end
+
+	def to_s
+		"Song: #{@name}-#{@artist} (#{@duration})"
+	end
+end
+
+class KaraokeSong < Song
+  def initialize(name, artist, duration, lyrics)
+    super(name, artist, duration) # get the result of method from parent class
+    @lyrics = lyrics
+  end
+
+  def duation=(newDuration)
+    @duration = newDuration
+  end
+
+  def durationInMinutes
+    @duration/60.0
+  end
+
+  def durationInMinutes=(durationInMinutes)
+    @duration = (durationInMinutes*60).to_i
+  end
+
+  def to_s
+    super + " [#{@lyrics}]"
+  end
+end
+
+aSong = KaraokeSong.new("Faded", "Alan Walker", 160, "I'm faded...")
+aSong.durationInMinutes=2.5
+puts aSong.name
+puts aSong.artist
+puts aSong.to_s
+
+aSong2 = KaraokeSong.new("Alone", "Alan Walker", 200, "Where are you now...")
+
+puts aSong.play
+puts aSong2.play
+puts aSong2.play
--- test.rb
+++
@@ -1,9 +0,0 @@
-a = %w( hello hi how are you )
-
-a.each { |word| puts word}
-
-3.times { print "*" }
-(1..5).each { |i| print i}
-
-# line = gets
-# puts line
